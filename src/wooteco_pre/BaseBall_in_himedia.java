176daac69a34e3ea3d1e22d990f4dc0ea578fc6e
Plastic_hong <zakkslash18@naver.com>
2022-11-06 19:31:30 +0900

    우테코 프리코스 2주차 미션 baseBall

--- src/wooteco_pre/BaseBall_in_himedia.java
+++ src/wooteco_pre/BaseBall_in_himedia.java
@@ -1,18 +1,27 @@
-package wooteco_pre;
+package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
-public class BaseBall {
+public class Application {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String correctAnswer = "425";
+        // TODO: 프로그램 구현
+        String correctAnswer = makeRandomNumStr();
+//        String correctAnswer = "123";
+//        String userAnswer = Console.readLine();
 
 
         boolean run = true;
         while (run) {
+
             run = runGame(correctAnswer);
+
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String next = sc.next();
+            String next = Console.readLine();
 
             if (next.equals("1")) {
                 run = true;
@@ -20,9 +29,10 @@ public class BaseBall {
                 System.out.println("종료합니다.");
                 run = false;
             } else {
-                System.out.println("잘못된 값을 입력하셨습니다.");
-               throw new IllegalArgumentException();
+                throw new IllegalArgumentException();
             }
+
+            correctAnswer = makeRandomNumStr();
 
         }
 
@@ -32,16 +42,17 @@ public class BaseBall {
     private static boolean runGame(String correctAnswer) {
         int ballCount;
         int strikeCount;
-        Scanner sc = new Scanner(System.in);
 
         boolean run = true;
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (run) {
             System.out.print("숫자를 입력해주세요 : ");
-            String userAnswer = sc.next();
+            String userAnswer = Console.readLine();
 
 //            입력값 검증
-            validate(userAnswer);
+            lengthCheck(userAnswer);
+            numberCheck(userAnswer);
+            duplicateCheck(userAnswer);
 
 //            계산
             strikeCount = getStrikeCount(correctAnswer, userAnswer);
@@ -51,17 +62,13 @@ public class BaseBall {
 //            결과 출력
             if (ballCount!=0){
                 System.out.print(ballCount + "볼 ");
-                if(strikeCount==0){
-                    System.out.println(ballCount + "볼 ");
-                }
             }
             if (strikeCount!=0){
                 System.out.println(strikeCount + "스트라이크 ");
             }
-            if(strikeCount==0||ballCount==0){
+            if(strikeCount==0&&ballCount==0){
                 System.out.println("낫싱");
             }
-
 
             if (strikeCount == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -73,46 +80,6 @@ public class BaseBall {
         return true;
     }
 
-    private static void validate(String userAnswer) {
-
-        lengthCheck(userAnswer);
-        typeCheck(userAnswer);
-        duplicateCheck(userAnswer);
-
-    }
-
-    private static void duplicateCheck(String userAnswer) {
-        char first = userAnswer.charAt(0);
-        char second = userAnswer.charAt(1);
-        char third = userAnswer.charAt(2);
-
-        if(first==second||second==third||first==third){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void typeCheck(String userAnswer) {
-
-        for (int i = 0; i < userAnswer.length(); i++) {
-            char c = userAnswer.charAt(i);
-            int numericValue = Character.getNumericValue(c);
-
-            if(numericValue<1||9<numericValue){
-                throw new IllegalArgumentException();
-            }
-
-        }
-
-    }
-
-    private static int lengthCheck(String userAnswer) {
-        int length = userAnswer.length();
-
-        if(length != 3){
-            throw new IllegalArgumentException();
-        }
-        return length;
-    }
 
 
     private static int getBallCount(String correctAnswer, String userAnswer) {
@@ -124,12 +91,11 @@ public class BaseBall {
             char c = userAnswer.charAt(i);
             int findIndex = correctAnswer.indexOf(c);
 
-            if (findIndex > -1 && findIndex != i) {
+            if (findIndex != i && findIndex >= 0) {
                 ballCount++;
             }
 
         }
-
         return ballCount;
     }
 
@@ -149,5 +115,57 @@ public class BaseBall {
         return strikeCount;
     }
 
+    private static void duplicateCheck(String userAnswer) {
+        char first = userAnswer.charAt(0);
+        char second = userAnswer.charAt(1);
+        char third = userAnswer.charAt(2);
+
+        if(first==second||second==third||first==third){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void numberCheck(String userAnswer) {
+        for (int i = 0; i < userAnswer.length(); i++) {
+            char c = userAnswer.charAt(i);
+            int numericValue = Character.getNumericValue(c);
+
+            if(numericValue<1||9<numericValue){
+                throw new IllegalArgumentException();
+            }
+
+        }
+    }
+
+
+    private static void lengthCheck(String userAnswer) {
+
+        if (userAnswer.length()!=3){
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+
+
+    private static String makeRandomNumStr() {
+        Set<String> correctAnswer = new HashSet<>();
+
+        while (correctAnswer.size() < 3) {
+            String str = Randoms.pickNumberInRange(1, 9) + "";
+            correctAnswer.add(str);
+        }
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : correctAnswer
+        ) {
+            stringBuilder.append(s);
+        }
+
+        String randomNumStr = String.valueOf(stringBuilder);
+
+        return randomNumStr;
+    }
 
 }
